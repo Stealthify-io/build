31190d148390f59992207e6b91df36ffe192f53c
princeinder <princeinder92@gmail.com>
2020-05-04 13:45:17 +0530

    first commit

---
+++ precache-manifest.2daea2e85a1000aae913c7193d4d5499.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae6fdf5d663f3545879916130164099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4659887771c01ecce43",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "ef41f9e7fafbb35f3a00",
+    "url": "/static/css/main.6f1dbcdb.chunk.css"
+  },
+  {
+    "revision": "a4659887771c01ecce43",
+    "url": "/static/js/2.4c5adb1d.chunk.js"
+  },
+  {
+    "revision": "5a08ba91138c20718c1ceed37824fee8",
+    "url": "/static/js/2.4c5adb1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef41f9e7fafbb35f3a00",
+    "url": "/static/js/main.84ce996a.chunk.js"
+  },
+  {
+    "revision": "91dd627788de88f2c435",
+    "url": "/static/js/runtime-main.722dec8e.js"
+  },
+  {
+    "revision": "f88ce666e1167a13fbfbcebe34389678",
+    "url": "/static/media/gift.f88ce666.jpg"
+  },
+  {
+    "revision": "be796275ec2778a37cec7935acce523d",
+    "url": "/static/media/iphone.be796275.jpg"
+  },
+  {
+    "revision": "0177a7c6ad81d596accced9dd4ab291b",
+    "url": "/static/media/phone.0177a7c6.jpg"
+  },
+  {
+    "revision": "777ba1585d63ac7e7237040754cd4ff5",
+    "url": "/static/media/stealth.777ba158.jpg"
+  }
+]);
